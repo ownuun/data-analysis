bf87c5a40a17094e7a32362fad1f6e84e7df61a5
Wonjun Seo <kk01057561299@gmail.com>
2025-05-13 15:26:09 +0900

    5.13 데탑

--- Desktop/개발 파일/데이터분석/car_data.py
+++ Desktop/개발 파일/데이터분석/car_data.py
@@ -6,4 +6,24 @@ import kagglehub
 path = kagglehub.dataset_download("yaminh/german-car-insights")
 
 df = pd.read_csv(path + '/gcar_data.csv')
-print(df)
+
+pd.set_option('display.max_columns', None)       # 열 전체 보이기
+pd.set_option('display.max_rows', None)          # 행 전체 보이기
+pd.set_option('display.max_colwidth', None)      # 문자열 잘리지 않게
+pd.set_option('display.expand_frame_repr', False)  # 옆으로 길게 출력
+
+# 🔍 전체 출력 (예: 상위 50행)
+df['price_in_euro'] = (
+    pd.to_numeric(
+        df['price_in_euro']
+        .astype(str)
+        .str.replace(',', '', regex=False)
+        .str.extract(r'(\d+\.?\d*)')[0],   # 숫자만 추출
+        errors='coerce'  # 변환 안 되는 값은 NaN 처리
+    )
+)
+
+
+df2 = df.groupby('brand')['price_in_euro'].mean().sort_values(ascending=False)
+print(df2['ferrari'])
+
